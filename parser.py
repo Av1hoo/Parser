d82e5c41d41dffa0364c55de4244d505835d3399
Av1hoo <tchvut@gmail.com>
2025-01-19 17:39:02 +0200

    Initial commit

---
+++ parser.py
@@ -0,0 +1,11 @@
+# 1. Parse output files from various instruments (different parsing logic required)
+# Automatically detect the appropriate parser based on file content.
+# Support both CSV and Excel file formats.
+# 2. Validate data based on provided business logic
+# 3. Store valid results in an SQLite database
+# 4. Provide a web interface for user interaction (is OK to skip CSS styling)
+
+# 1. Parser
+# 1.1. Parse CSV files
+
+# 1.2. Parse Excel files
